a04ebbc49e83adcc51f40a017ef087a4fb3aa215
Surendra Das <surendradas9813@gmail.com>
2021-02-15 22:36:52 +0530

    Database is added

--- app.js
+++ app.js
@@ -2,59 +2,112 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 
+// requiring mongoose for database
+const mongoose = require("mongoose");
+
+
 const app = express();
 
 //for using static file like css external file
 app.use(express.static("public"));
 
 //for getting the form data throught the html page
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //for templating
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
+
+// connecting to the database
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+
+// creating schema
+const itemsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true
+    }
+})
+
+// creating mongoose model
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist"
+});
+
+const item2 = new Item({
+    name: "hit the + button to add a new item"
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
 
 
-var list = ["Buy Food","Cook Food","Eat Food"];
-var workList = [];
 
-app.get("/",(req,res)=>{
+
+
+app.get("/", (req, res) => {
 
     var today = new Date();
 
     var options = {
         weekday: "long",
-       
+
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US",options);
-    
-    res.render("list",{listTitle:day,listItems:list});
-    
+    var day = today.toLocaleDateString("en-US", options);
+
+    Item.find({}, (err, items) => {
+        if (items.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
+                    console.log();
+                }
+                else {
+                    console.log("successfully added!");
+                }
+            });
+            res.redirect("/")
+        }
+        else {
+            res.render("list", { listTitle: day, listItems: items });
+        }
+    });
+
+
 
 });
 
-app.post("/",(req,res)=>{
- let item = req.body.newItem;
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
 
- if(req.body.btn === "Work List"){
-    workList.push(item);
-    res.redirect("/work");
- }
-else{   
-    list.push(item);
+    const newItem = new Item({
+        name: item
+    });
+
+    newItem.save();
     res.redirect("/");
-}
-    
-})
 
-
-app.get("/work",(req,res)=>{
-    res.render("list",{listTitle:"Work List",listItems:workList});
 });
 
 
-app.listen(3000,()=>{
+app.post("/delete", (req, res) => {
+    const toDelete = req.body.checkbox;
+
+    Item.findByIdAndRemove(toDelete, (err) => {
+        if (!err) {
+            console.log("Successfully Deleted check item!");
+            res.redirect("/");
+        }
+    })
+});
+
+
+app.listen(3000, () => {
     console.log("server is up and running on port 4000");
 });
